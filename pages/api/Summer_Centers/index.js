221cedec04bdc2ed9b0763b2815bc2899d3d8a81
beylax <manuel.baldoni04@gmail.com>
2022-08-31 19:34:25 +0200

    Aggiornato json response

--- pages/api/Summer_Centers/index.js
+++ pages/api/Summer_Centers/index.js
@@ -15,7 +15,7 @@ export default async function getAllUsers(req, res) {
 		res.setHeader("Access-Control-Max-Age", "1800");
 		res.setHeader("Access-Control-Allow-Headers", "content-type");
 		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-        res.status(200).json({ results: data });
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: error.message});
     }
--- pages/api/Users/[username].js
+++ pages/api/Users/[username].js
@@ -15,7 +15,7 @@ export default async function getAllUsers(req, res) {
 		res.setHeader("Access-Control-Max-Age", "1800");
 		res.setHeader("Access-Control-Allow-Headers", "content-type");
 		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-        res.status(200).json({ results: data[0] });
+        res.status(200).json(data[0]);
     } catch (error) {
         res.status(500).json({ error: error.message});
     }
